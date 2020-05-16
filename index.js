94243882e8e6909c4025fe03bec991c357e6d8eb
Bawan Majid <bawanmajid23@gmail.com>
2020-05-16 07:34:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,6 +9,14 @@ console.log(dodger.style.bottom);
 dodger.style.bottom = "100px";
 dodger.style.bottom = "0px";
 
+document.addEventListener("keydown", function(e) {
+  if (e.key === "ArrowLeft") {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+ 
+    dodger.style.left = `${left - 1}px`;
+  }
+});
 
 function moveDodgerLeft() {
   let leftNumbers = dodger.style.left.replace("px", "");
