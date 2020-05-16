cd81aecdaa7d04b40c527832dcf9d4966df925d7
Bawan Majid <bawanmajid23@gmail.com>
2020-05-16 07:27:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ let dodger = document.getElementById("dodger");
 dodger.style.backgroundColor = "#FF69B4";
 console.log(dodger.style.left);
 console.log(dodger.style.bottom); 
+dodger.style.bottom = "100px";
